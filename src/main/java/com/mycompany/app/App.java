cf64ee9c8fe63accfec8b2bb7f7476964a5df75f
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2022-09-04 01:57:42 +0700

    simple response

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,5 +1,6 @@
 package com.mycompany.app;
 
+
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
@@ -15,44 +16,14 @@ public class App
     public App() {}
 
     static class MyHttpHandler implements HttpHandler {    
-        @Override    
-        public void handle(HttpExchange httpExchange) throws IOException {
-            String requestParamValue = null; 
-            if ("GET".equals(httpExchange.getRequestMethod())) { 
-            requestParamValue = handleGetRequest(httpExchange);
-            } else if("POST".equals(httpExchange)) { 
-            requestParamValue = handlePostRequest(httpExchange);        
-            }  
-            handleResponse(httpExchange,requestParamValue); 
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            String response = "This is the response";
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
         }
-        
-        private String handleGetRequest(HttpExchange httpExchange) {
-                    return httpExchange.
-                            getRequestURI()
-                            .toString()
-                            .split("\\?")[1]
-                            .split("=")[1];
-        }
-
-        private void handleResponse(HttpExchange httpExchange, String requestParamValue)  throws  IOException {
-                    OutputStream outputStream = httpExchange.getResponseBody();
-                    StringBuilder htmlBuilder = new StringBuilder();
-                    htmlBuilder.append("<html>").
-                            append("<body>").
-                            append("<h1>").
-                            append("Hello ")
-                            .append(requestParamValue)
-                            .append("</h1>")
-                            .append("</body>")
-                            .append("</html>");
-                    // encode HTML content 
-                    String htmlResponse = StringEscapeUtils.escapeHtml4(htmlBuilder.toString());
-                    // this line is a must
-                    httpExchange.sendResponseHeaders(200, htmlResponse.length());
-                    outputStream.write(htmlResponse.getBytes());
-                    outputStream.flush();
-                    outputStream.close();
-                }
     }
 
 
